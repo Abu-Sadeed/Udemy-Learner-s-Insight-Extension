5aa09236c9e6a766c9f08f41a373a6ec245c619b
Md. Abu Sadeed <abu.sadeed@reddotdigitalit.com>
2024-10-13 01:17:36 +0600

    extension can manipulate backgroud color

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
 const App = () => {
-  return <div className="w-full h-full bg-yellow-400">App</div>;
+  const changeBackgroundColor = async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+    });
+
+    if (!tab) {
+      console.error('No active tab found');
+      return;
+    }
+
+    const tabId = tab.id;
+    chrome.scripting.executeScript({
+      target: { tabId: tabId! },
+      func: () => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        const backgroundColor = `#${randomColor}`;
+        document.body.style.backgroundColor = backgroundColor;
+        console.log(`Changed background color to ${backgroundColor}`);
+      },
+    });
+  };
+
+  return (
+    <div className="w-full h-screen bg-yellow-400 flex items-center justify-center">
+      <button
+        onClick={changeBackgroundColor}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Change Background Color
+      </button>
+    </div>
+  );
 };
 
 export default App;
